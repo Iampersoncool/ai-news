045a3c57a0b2dc0dd89e41e1c38e0e7b01a8e8b4
Iampersoncool <86845476+Iampersoncool@users.noreply.github.com>
2025-04-27 14:48:26 -0700

    Enhancement: Make getDocumentFromHTML nonstatic

--- packages/scraper/tests/news-scraper-service.test.ts
+++ packages/scraper/tests/news-scraper-service.test.ts
@@ -1,16 +1,21 @@
 import { readFileSync } from 'node:fs';
 import { describe, expect, test } from 'vitest';
+
+import HeaderService from '../src/services/HeaderService';
 import NewsScraperService from '../src/services/NewsScraperService';
+
+const headerService = new HeaderService();
+
+const newsScraperService = new NewsScraperService(headerService, {
+  parseNoneClosedTags: true,
+});
 
 describe('NewsScraperService', () => {
   test('getDocumentFromHTML returns valid document with valid document body', () => {
-    const document = NewsScraperService.getDocumentFromHTML(
+    const document = newsScraperService.getDocumentFromHTML(
       readFileSync(
         'packages/scraper/news/news-scraper-test-pbs.html'
-      ).toString(),
-      {
-        parseNoneClosedTags: true,
-      }
+      ).toString()
     );
 
     expect(document.querySelector('body')).not.toBeNull();
